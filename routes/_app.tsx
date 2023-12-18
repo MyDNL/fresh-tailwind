eb8f67ad5910629a18a0b4fb8c8687b76c2f143f
soner <sonermeydanli@gmail.com>
2023-12-18 13:22:22 +0300

    manifest added

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,3 +1,5 @@
+// import { Head } from "https://deno.land/x/fresh@1.6.1/runtime.ts";
+// import { pwa-update } from  "npm:@pwabuilder/pwaupdate";
 import { type PageProps } from "$fresh/server.ts";
 import { NavBar } from "../components/NavBar.tsx";
 
@@ -6,9 +8,10 @@ export default function App({ Component }: PageProps) {
     <html data-theme="light">
       <head>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>fresh-tailwind</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+        <title>Countdown 2024</title>
         <link rel="stylesheet" href="/styles.css" />
+        <link rel="manifest" href="manifest.json" />
       </head>
       <body>
         <NavBar />
